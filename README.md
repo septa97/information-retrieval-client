d953e8ac5f6b6bf61b5f0fb084a3abe71549aee4
septa97 <jancharles.adona@gmail.com>
2017-04-18 01:02:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+CMSC 191 Information Retrieval Client
+=====================================
+
+> Information Retrieval Exercise
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:8080
+$ quasar dev
+
+# build for production with minification
+$ quasar build
+
+# lint code
+$ quasar lint
+```
---
+++ src/components/Index.vue
@@ -0,0 +1,88 @@
+<template>
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="0">
+        Information Retrieval
+      </q-toolbar-title>
+    </div>
+
+    <div class="layout-view">
+      <div class="layout-padding">
+        <q-search v-model="searchQuery" @input="search"></q-search>
+        <table class="q-table">
+          <thead>
+            <tr>
+              <th>Rating</th>
+              <th>Tail</th>
+              <th>Title</th>
+              <th>Review</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="result in results">
+              <td>{{ result.rating }}</td>
+              <td>{{ result.tail }}</td>
+              <td>{{ result.title }}</td>
+              <td>{{ result.review }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+  'use strict'
+
+  import { Loading } from 'quasar'
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        searchQuery: '',
+        results: []
+      }
+    },
+    methods: {
+      search () {
+        Loading.show({
+          delay: 100,
+          message: 'Searching...',
+          spinner: 'dots',
+          spinnerSize: 150
+        })
+
+        const config = {
+          method: 'get',
+          baseURL: 'http://localhost:8000/api',
+          url: `/search?search_query=${this.searchQuery}`
+        }
+
+        axios(config)
+          .then(response => {
+            this.results = response.data
+            Loading.hide()
+          })
+          .catch(error => {
+            throw new Error(error)
+          })
+      }
+    }
+  }
+</script>
+
+<style lang="styl">
+.logo-container
+  width 192px
+  height 268px
+  perspective 800px
+  position absolute
+  top 50%
+  left 50%
+  transform translateX(-50%) translateY(-50%)
+.logo
+  position absolute
+  transform-style preserve-3d
+</style>
